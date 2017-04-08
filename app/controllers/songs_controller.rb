96df70e39bbf67c5981783e3301e48dea4c71fa1
Usman Pervaiz <de.usman@yahoo.com>
2017-04-08 04:01:27 -0400

    Completed, but confusing, the last test passes without updating Genre

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,26 +14,29 @@ class SongsController < Sinatra::Base
   	erb :'songs/index'
   end
 
+
   get '/songs/new' do 
   	erb :'songs/new'
    
   end
 
+
   get "/songs/:slug" do 
   	@song = Song.find_by_slug(params[:slug])
-  #binding.pry
-	erb :'songs/show'
+	  erb :'songs/show'
   end
+
 
   post "/songs" do
   	artist = Artist.find_or_create_by(name: params[:'Artist Name'])
   	genre_id = params[:genres][0].to_i
-	song = Song.create(name: params[:Name], artist_id: artist.id)
- 	song.genres << Genre.find(genre_id)
- 	song.save
- 	flash[:message] = "Successfully created song."
+	  song = Song.create(name: params[:Name], artist_id: artist.id)
+ 	  song.genres << Genre.find(genre_id)
+ 	  song.save
+  	flash[:message] = "Successfully created song."
   	redirect to "/songs/#{song.slug}"
   end
+
 
   get '/songs/:slug/edit' do
   	@song = Song.find_by_slug(params[:slug])
@@ -41,12 +44,18 @@ class SongsController < Sinatra::Base
   	erb :'songs/edit'
   end
 
+
   patch '/songs/:slug' do
   	song = Song.find_by_slug(params[:slug])
-    song.update(params[:song])   	
+
+    if (!params["Artist Name"].empty?)
+      song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    end
+
+    song.genres.push(Genre.find(params[:genres][0].to_i))
     song.save
+
     flash[:message] = "Successfully updated song."
-  #binding.pry
     redirect to "/songs/#{song.slug}"
   end 
 
